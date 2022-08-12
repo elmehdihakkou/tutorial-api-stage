fe0d3a553e4e6126febe2647b9fdaeadeaf9e126
elmehdihakkou <elmehdihakkou@gmail.com>
2022-08-12 13:16:55 +0100

    adding investment_status

--- src/main/java/com/example/blondiebytes/tutorial/Controller/GreetingController.java
+++ src/main/java/com/example/blondiebytes/tutorial/Controller/GreetingController.java
@@ -1,12 +1,17 @@
 package com.example.blondiebytes.tutorial.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
     @RequestMapping("/greeting")
-    public String getGreeting(){
-        return "Hi it's me Midou adn I like gaming";
+    public int getGreeting(@RequestParam(value = "investment_status", defaultValue = " ") boolean investment_status){
+        if (investment_status){
+            return 1;
+        } else{
+            return 0;
+        }
     }
 }
